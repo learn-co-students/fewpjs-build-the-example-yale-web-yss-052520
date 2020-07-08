7d2f3031e1e92eb1017e70144273902164228a80
ZIming Mao <ziming.mao@yale.edu>
2020-07-08 18:16:43 -0400

    Done.

--- main.js
+++ main.js
@@ -4,8 +4,37 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let elements = document.getElementsByClassName("like");
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener('click', my_func);
+}
 
+function my_func(e){
+  if(e.target.innerText == EMPTY_HEART){
+  mimicServerCall().then(
+    res => check_result(res,e)
+  ).catch(function(error){
+    // console.log("asdf")
+    const display = document.querySelector('.hidden')
+    display.classList.remove("hidden")
+    document.querySelector('#modal-message').innerText = error
+    setTimeout(function(){ display.classList.add("hidden"); }, 5000);
+  })}
+  if (e.target.innerText == FULL_HEART){
+    e.target.innerText = EMPTY_HEART
+    e.target.classList.remove("activated-heart")
+  }
+}
+function check_result(res,e){
+  if (res == "Pretend remote server notified of action!"){
+    
+      e.target.innerText = FULL_HEART
+      e.target.classList.add("activated-heart")
+    
+    
 
+  }
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
