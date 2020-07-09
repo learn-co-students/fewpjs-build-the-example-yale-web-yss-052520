98ea3763b13803ce96bd50d5fd36267d3d81042c
nicolewang2000 <nicole.wang@yale.edu>
2020-07-09 18:59:39 -0400

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const warning = document.querySelector("div#modal")
+warning.classList.add("hidden")
 
-
-
+document.addEventListener('DOMContentLoaded', () => {
+  const allHearts = document.querySelectorAll("span.like-glyph")
+  allHearts.forEach(heart =>{
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+      .then(function(response) {
+        if (heart.innerText == FULL_HEART){
+          heart.innerText = EMPTY_HEART
+          heart.classList.remove("activated-heart")
+        }
+        else {
+          heart.innerText = FULL_HEART
+          heart.classList.add("activated-heart")
+        }
+      })
+      .catch(error =>{
+        warning.classList.remove('hidden')
+        warning.children[1].innerText = error
+        setTimeout(() => warning.classList.add("hidden"), 5000)
+      }) 
+    })
+  })
+})
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
