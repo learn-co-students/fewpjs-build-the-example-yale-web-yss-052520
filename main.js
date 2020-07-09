a97a4d07a3d31f1a4a3d05a5be7e1a6b60b62457
Drew Beckmen <drew.beckmen@yale.edu>
2020-07-08 21:31:54 -0400

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const allHearts = document.getElementsByClassName("like")
+
+// takes in parameter e, which is the node event happend on
+function changeLike(e) {
+  const likeElement = e.target 
+  mimicServerCall("http://mimicServer.example.com")
+  .then(function(response) {
+    if (likeElement.innerText == FULL_HEART) {
+      likeElement.innerText = EMPTY_HEART 
+      likeElement.className = "" 
+    }
+    else {
+      likeElement.innerText = FULL_HEART 
+      likeElement.className = "activated-heart"
+    }
+    
+  })
+  .catch(function(error) {
+    document.getElementById("modal").className = "notHidden"
+  })
+}
 
 
 
+// Use for..of with HTMLCollection
+// Second argument is the function
+for (const post of allHearts) {
+  post.addEventListener("click", changeLike)
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
