f8b6b098b475605615f41d994bff16f172825c63
s-dney <littlebirdsword@gmail.com>
2020-07-09 21:51:05 -0700

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
 // Your JavaScript code goes here!
 
+const hearts = document.querySelectorAll(".like-glyph")
+const errorModal = document.querySelector("#modal")
 
+hearts.forEach( heart => {
+
+  heart.addEventListener("click", () => {
+    mimicServerCall()
+    .then(() => {
+      if (heart.innerText == EMPTY_HEART) {
+        heart.innerText = FULL_HEART
+        heart.classList.add("activated-heart")
+      }
+      else {
+        heart.innerText = EMPTY_HEART
+        heart.classList.remove("activated-heart")
+      }
+    })
+    .catch((error) => {
+      errorModal.innerText = error
+      errorModal.classList.remove("hidden")
+      setTimeout(() => {errorModal.classList.add("hidden")}, 5000)
+    })
+    
+
+  })
+
+})
 
 
 //------------------------------------------------------------------------------
