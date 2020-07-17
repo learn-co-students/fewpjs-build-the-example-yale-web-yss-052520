d33cb587a305c1ebd3f197ee339738ca55f22a76
davidyu7 <david.yu@yale.edu>
2020-07-17 11:29:42 -0400

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.querySelector("div#modal")
 
 // Your JavaScript code goes here!
 
-
+posts = document.querySelectorAll("article.media-post")
+posts.forEach(post => {
+  const likeBtn = post.querySelector("li.like")
+  const likeGlyph = likeBtn.querySelector("span.like-glyph")
+  likeBtn.addEventListener("click", function() {
+    mimicServerCall()
+    .then(() => {
+      if (likeGlyph.className.includes("activated-heart")){
+        likeGlyph.innerText = EMPTY_HEART
+        likeGlyph.className = "like-glyph"
+      } else {
+        likeGlyph.innerText = FULL_HEART
+        likeGlyph.className += " activated-heart"
+      }
+    })
+    .catch(error => {
+      errorModal.className = ""
+      errorModal.querySelector("p#modal-message").innterText = error.message
+      setTimeout(function() {errorModal.className = "hidden"}, 5000)
+    })
+  })
+})
 
 
 //------------------------------------------------------------------------------
