72fd69110301ff87f42fb7b6b0572a23878d34cd
tbartelsmeyer <47281160+tbartelsmeyer@users.noreply.github.com>
2020-07-25 21:17:34 -0500

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,27 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+let likeBtns = document.querySelectorAll('.like')
+likeBtns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    mimicServerCall()
+      .then( res => {
+        if (btn.className == 'like') {
+          btn.innerText = `Like! ${FULL_HEART}`
+          btn.className = 'activated-heart'
+        } else if (btn.className == 'activated-heart') {
+          btn.innerText = `Like! ${EMPTY_HEART}`
+          btn.className = 'like'
+        }
+      })
+        .catch( error => {
+          modal = document.querySelector('#modal')
+          modal.className = ""
+          modal.innerText = "Random server error. Try again."
+          setTimeout(function(){ modal.className = "hidden" }, 5000)
+        })
+  })
+})
 
 
 
